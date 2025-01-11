5ad96aed25bac6fdd485462cba05733a702ffaf0
Amrit <Amrit.09@icloud.com>
2025-01-11 13:04:17 -0800

    Hidden Accessories Table

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const urlGithub = "https://github.com/ayoamrit/findit";
+const accessorySection = document.getElementById('accessories-section');
+const searchButton = document.getElementById('search-button');
 
 function redirectGithub(){
     window.open(urlGithub, '_blank');
