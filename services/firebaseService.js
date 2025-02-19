0f3c6ff8ee651bd5cbc5cc3767c77b58095df1d6
Amrit Dhaliwal <Amrit78887@gmail.com>
2025-02-18 18:12:33 -0800

    Environmental Variables Added

--- services/firebaseService.js
+++ services/firebaseService.js
@@ -1,10 +1,25 @@
 const firebaseAdmin = require("firebase-admin");
 const path = require("path");
+require("dotenv").config();
 
-const serviceAccount = require(path.join(__dirname, '../firebaseDatabaseAdminFile.json'));
+const serviceAccount = {
+    "type": "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
+
 firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount),
-    databaseURL: "https://findit-database-3526e-default-rtdb.firebaseio.com"
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+    //databaseURL: "https://findit-database-3526e-default-rtdb.firebaseio.com"
 });
 
 const firebaseDatabase = firebaseAdmin.database();
