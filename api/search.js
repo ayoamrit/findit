826119bb277dabc86b47fcaffdffe0340c5eb118
Amrit Dhaliwal <Amrit78887@gmail.com>
2025-02-19 18:53:23 -0800

    Test V

--- api/search.js
+++ api/search.js
@@ -2,24 +2,6 @@ const { getModel, getAllModels, addModel, removeModel } = require('../services/f
 
 export default function handler(req, res) {
     if (req.method === 'GET') {
-        const modelNumber = req.query.model;
-
-        if (!modelNumber) {
-            return res.status(400).json({ error: "Model number is required" });
-        }
-
-        const searchedData = getModel(modelNumber);
-        if (searchedData) {
-
-            const accessories = searchedData.accessories?searchedData.accessories.split(", ").map(item => item.trim()) : [];
-            res.json({
-                accessories,
-                sku: searchedData.sku,
-                modelName: searchedData.modelName,
-                url: searchedData.url
-            });
-        } else {
-            res.status(404).json({ error: "Model not found" });
-        }
+        return res.json({"Name": "Amrit Dhaliwal"});
     }
 }
